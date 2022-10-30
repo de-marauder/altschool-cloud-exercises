dac223d469fa0dbf74b27a98a1b3ea2b205b95dd
de-marauder <ezikegodson@gmail.com>
2022-10-30 11:07:28 +0100

    bash script update

--- exam-project/postgres-config.sh
+++ exam-project/postgres-config.sh
@@ -1,38 +1,22 @@
 #!/bin/bash -ex
 
-# This script is for use with the DevOps Challenge of installing PostgreSQL 9.6 on to a provisioned AWS EC2 instance running Ubuntu.
+# This script is for installing the latest version of PostgreSQL on to a provisioned AWS EC2 instance running Debian.
 
 # This script will perform the following steps:
-# 1. Set variables such as $packages, $rfolder, $dfolder, $gitloc, $sysuser, $logfile, and $laraveldbscript
+# 1. Set variables such as $packages, $logdir, $logfile, and $laraveldbscript
 # 2. Install packages based off of $packages.
-# 3. Create directory: /postgres and other required.
-# 4. Create system user 'postgres'.
-# 5. Pull PostgreSQL from Git depot and confirm it is correct: git clone git://git.postgresql.org/git/postgresql.git.
-# 6. Install PostgreSQL. ensuring the data files are stored in $dataFolder.
-# 7. Start the PostgreSQL service using the pg_ctl command.
-# 8. Run create_hello.sql script.
-# 9. Run '/postgres/bin/psql -c 'select * from hello;' -U user hello_postgres;' against newly created DB and test for succesful response.
+# 3. Start the PostgreSQL service using the systemctl command.
+# 4. Run create-laravel-db.sql.sql script.
 
 # Section 1 - Variable Creation
 
-# echo "Creating variables for use throughout the PSQL installation process"
 # $packages is an array containing the dependencies for PostgreSQL
-# packages=('git' 'gcc' 'tar' 'gzip' 'libreadline5' 'make' 'zlib1g' 'zlib1g-dev' 'flex' 'bison' 'perl' 'python3' 'tcl' 'gettext' 'odbc-postgresql' 'libreadline6-dev')
 packages=('postgresql' 'postgresql-contrib' 'python3-psycopg2')
-# $rfolder is the install directory for PostgreSQL
-# rfolder='/postgres'
-# rfolder="~"
-# $dfolder is the root directory for various types of read-only data files
-# dfolder='/postgres/data'
-# dfolder="~/data"
-# $gitloc is the location of the PosgreSQL git repo
-# gitloc='git://git.postgresql.org/git/postgresql.git'
-# $sysuser is the system user for running PostgreSQL
-# sysuser='postgres'
 # $laraveldbscript is the sql script for creating the PSQL user and creating a database.
 laraveldbscript='/home/postgres/create-laravel-db.sql'
-# $logfile is the log file for this installation.
+# $logdir is the log file directory for this installation.
 logdir='/var/log/script-install'
+# $logfile is the log file for this installation.
 logfile=${logdir}'/psqlinstall-log'
 
 mkdir -p $logdir
@@ -45,22 +29,20 @@ touch $logfile
 sudo apt-get update -y >> $logfile
 
 # This for-loop will pull all packages from the package array and install them using apt-get
-# echo "Installing PostgreSQL dependencies"
+
 sudo apt-get install ${packages[@]} -y >> $logfile
 
 
 # Section 3 - Start postgresql service
+
 sudo systemctl restart postgresql
 
-# echo "Wait for PostgreSQL to finish starting up..."
+# "Wait for PostgreSQL to finish starting up..."
 sleep 5
 
+# Section 4
+
 # The create-laravel-db.sql script is ran to create the user and database.
-# echo "Running script"
+
 sudo -u postgres psql -f $laraveldbscript
 
-
-# # Section 9 - hello_postgres is queried
-
-# echo "Querying the newly created table in the newly created database."
-# /postgres/bin/psql -c 'select * from hello;' -U laravel_user hello_postgres;
