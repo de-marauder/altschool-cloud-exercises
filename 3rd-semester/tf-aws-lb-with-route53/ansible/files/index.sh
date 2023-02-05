f27ce46a03b91901639a2773fba0651cbab2a993
de-marauder <ezikegodson@gmail.com>
2023-02-05 11:40:13 +0100

    updated ansible webpage script

--- 3rd-semester/tf-aws-lb-with-route53/ansible/files/index.sh
+++ 3rd-semester/tf-aws-lb-with-route53/ansible/files/index.sh
@@ -3,5 +3,6 @@ sudo timedatectl set-timezone Africa/Lagos
 
 export t="$(date) $(timedatectl | head -4 | tail -1 | awk -F ': ' '{print $2}')"
 
-echo "<h1>$(hostname)</h1>" >/var/www/html/index.html
+echo "<h1>Obiajulu Ezike</h1> a.k.a. <i>(de-marauder)</i>" >/var/www/html/index.html
+echo "<h1>$(hostname)</h1>" >>/var/www/html/index.html
 echo "<h2>$t</h2>" >>/var/www/html/index.html
